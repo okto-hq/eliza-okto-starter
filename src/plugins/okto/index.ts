8a931a0ce50938da0c0a3ed977f8c1b5970a7d71
Jovian Dsouza <dsouzajovian123@gmail.com>
2025-02-03 13:15:46 +0530

    feat: add get account action

--- src/plugins/okto/index.ts
+++ src/plugins/okto/index.ts
@@ -14,6 +14,7 @@ import { GetSupportedNetworksResponseData, Order, UserNFTBalance, UserPortfolioD
 import { tokenTransfer, nftTransfer, evmRawTransaction } from "@okto_web3/core-js-sdk/userop";
 import { NFTTransferIntentParams, RawTransactionIntentParams, TokenTransferIntentParams, Token, Wallet } from "./types.ts";
 import { getPortfolioAction } from "./actions/getPortfolioAction.ts";
+import { getAccountAction } from "./actions/getAccountAction.ts";
 
 export class OktoPlugin implements Plugin {
   readonly name: string = "okto";
@@ -61,6 +62,7 @@ export class OktoPlugin implements Plugin {
 
   actions: Action[] = [
     getPortfolioAction(this),
+    getAccountAction(this),
     // transferTokensAction(this),
     // getWalletsAction(this),
     // orderHistoryAction(this),
